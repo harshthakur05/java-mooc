08cdf3199d0199bf2761040ebc3bb5d1dd3c2b4e
Harsh Thakur <harsh.thakur8578@gmail.com>
2025-12-13 17:41:04 +0530

    timer.java included

---
+++ part5/excercise1/Timer.java
@@ -0,0 +1,39 @@
+package part5.excercise1;
+
+public class Timer {
+    private ClockHand hundredth;
+    private ClockHand seconds;
+
+    public Timer(){
+        this.hundredth = new ClockHand(100);
+        this.seconds = new ClockHand(60);
+    }
+    public void advance(){
+        this.hundredth.advance();
+
+        if (this.hundredth.value() ==0){
+            
+                this.seconds.advance();
+            
+        }
+    }
+    public String toString(){
+        return hundredth + ":" + seconds;
+    }
+
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+
+        while (true) {
+            System.out.println(timer);
+            timer.advance();
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+}
